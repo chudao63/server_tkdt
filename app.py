745b6492785147ab5f5e1ce7673c26e3036932c6
chudao63 <chudao63@gmail.com>
2022-09-21 21:38:41 +0700

    add mqtt

--- app.py
+++ app.py
@@ -3,38 +3,47 @@
 # import serial
 # # import pyrebase
 # from time import sleep
+from email import message
+from glob import escape
 from random import random
+from re import A
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 # from threading import Thread
 import time
 import _thread
 import random
-# __all__ = ("error", "LockType", "start_new_thread", "interrupt_main", "exit", "allocate_lock", "get_ident", "stack_size", "acquire", "release", "locked")
-
-# eventlet.monkey_patch()
+from flask_mqtt import Mqtt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+app.config['MQTT_BROKER_URL'] = "broker.hivemq.com"
+app.config['MQTT_BROKER_PORT'] = 1883
 
 
-fs = {"Moment_1": 34, "Moment_2": 45, "Moment_3": 10,"Moment_4": 98 }
+topic = "/quang_trinh_an_cut"
+topic2 = "/quang_trinh_an_cut2"
 
-object_mm1 = fs["Moment_1"]
-object_mm2 = fs["Moment_2"]
-object_mm3 = fs["Moment_3"]
-object_mm4 = fs["Moment_4"]
+mqtt_client = Mqtt(app)
 
-data = {"Moment 1" : object_mm1,
-        "Moment 2" : object_mm2,
-        "Moment 3" : object_mm3,
-        "Moment 4" : object_mm4 
-        }
-@socketio.on('vehicle message')
-def broadcast_vehicle(data):
-    print(data)
-    emit('vehicles', data, broadcast=True)
+@mqtt_client.on_connect()
+def handle_connect(client, userdate,flags,rc):
+  if rc == 0:
+    print("connect successfully") 
+    mqtt_client.subscribe(topic)
+    mqtt_client.subscribe(topic2)
+
+  else:
+    print("deo connect dc")
+
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+  data = dict(topic = message.topic,
+              payload = message.payload.decode()
+              )
+  print(data)
+
 
 
 @app.route('/api/event', methods=['POST'])
@@ -42,6 +51,8 @@ def event():
     data = request.json
     emit('event', data, broadcast=True, namespace='/')
     return 'OK'
+
+
 def background_thread():
     """Example of how to send server generated events to clients."""
    
@@ -57,15 +68,22 @@ data = {
         "Moment_3": random.randint(1, 100),
         "Moment_4": random.randint(101, 299)
        }
+
 @app.route('/')
 def index():
-  # _thread.start_new_thread( background_thread, () )
   return data
 
 @app.route("/sensor")
-def scan_sensor():
+def send_in4_sensor():
   return {"id": f"sensor_S0000{random.randint(1, 10)}","type": "sensor", "value": random.randint(20, 100), "unit": "percentage"}
 
+@app.route("/scan")
+def scan_sensor():
+  mqtt_client.publish(topic=topic,payload = "scan_sensor")
+  return "Scan sensor successfully"
+
+
 if __name__ == '__main__':
+  _thread.start_new_thread( background_thread, () )
   socketio.run(app)
 
